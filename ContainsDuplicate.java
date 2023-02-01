e3f21f6427c8559653459235e8446c8b72ae3038
Rohuu <rohuu99022@gmail.com>
2023-02-01 11:51:55 +0530

    check if any value appears at least twice in the array

---
+++ ContainsDuplicate.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 1 };
+        boolean ans = isHavingDuplicateValues(nums);
+        System.out.println(ans);
+    }
+
+    static boolean isHavingDuplicateValues(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) != 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
