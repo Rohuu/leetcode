1ef4c34fbdae9a986e5940c82a3add70e4129177
Rohuu <rohuu99022@gmail.com>
2023-02-04 21:52:46 +0530

    problem: Find array product except self in a non zero array

---
+++ ArrayProductExceptSelf.java
@@ -0,0 +1,31 @@
+// Given an integer non zero array nums, return an array answer such that answer[i] is equal to the product 
+// of all the elements of nums except nums[i].
+
+import java.util.Arrays;
+
+public class ArrayProductExceptSelf {
+    public static void main(String[] args) {
+        // input Non zero array
+        int[] nums = { 1, 2, 3, 0, 5 };
+        int[] ans = returnArrayWithProductExceptSelf(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] returnArrayWithProductExceptSelf(int[] nums) {
+        int[] arr = new int[nums.length];
+
+        if (nums.length == 0) {
+            return arr;
+        }
+
+        int productOfNumsArray = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            productOfNumsArray = productOfNumsArray * nums[i];
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            arr[j] = productOfNumsArray / nums[j];
+        }
+        return arr;
+    }
+}
