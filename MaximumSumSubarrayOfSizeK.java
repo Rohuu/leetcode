cbdabb0a776d3be35f6c79e03a27f093265a3d19
Rohuu <rohuu99022@gmail.com>
2023-02-06 14:37:08 +0530

    Maximum Sum Subarray of Size K using sliding window approach

---
+++ MaximumSumSubarrayOfSizeK.java
@@ -0,0 +1,24 @@
+public class MaximumSumSubarrayOfSizeK {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 5, -9, -89, -5786, 0, 2, 1, 0, 23, 0 };
+        int ans = maximumSum(arr, 3);
+        System.out.println(ans);
+    }
+
+    static int maximumSum(int[] arr, int k) {
+        int i = 0;
+        int sum = 0;
+        int maxSum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum = sum + arr[j];
+            if (j >= k - 1) {
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+                sum = sum - arr[i];
+                i++;
+            }
+        }
+        return maxSum;
+    }
+}
