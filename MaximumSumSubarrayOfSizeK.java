0ae6f610af49910a55f2af7aeb0d8db41d6e7f1f
Rohuu <rohuu99022@gmail.com>
2023-02-08 15:02:19 +0530

    modified input

--- MaximumSumSubarrayOfSizeK.java
+++ MaximumSumSubarrayOfSizeK.java
@@ -1,7 +1,7 @@
 public class MaximumSumSubarrayOfSizeK {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 4, 5, -9, -89, -5786, 0, 2, 1, 0, 23, 0 };
-        int ans = maximumSum(arr, 3);
+        int ans = maximumSum(arr, 5);
         System.out.println(ans);
     }
 
@@ -10,13 +10,13 @@ public class MaximumSumSubarrayOfSizeK {
         int sum = 0;
         int maxSum = 0;
         for (int j = 0; j < arr.length; j++) {
-            sum = sum + arr[j];
+            sum = sum + arr[j]; // add the next element
             if (j >= k - 1) {
                 if (sum > maxSum) {
                     maxSum = sum;
                 }
-                sum = sum - arr[i];
-                i++;
+                sum = sum - arr[i]; // subtract the element going out
+                i++; // slide the window ahead
             }
         }
         return maxSum;
