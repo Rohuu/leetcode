c679733a1ef344853408809865ba515824c2a28b
Rohuu <rohuu99022@gmail.com>
2023-07-26 23:18:05 +0530

    Python learning: DAY 0

--- python-coding/testing.py
+++ python-coding/testing.py
@@ -1,1 +1,24 @@
-print("hello there");
+# print
+print("hello there")
+
+# loop
+for i in range(2, 5):
+    print(i)
+
+# nested loop
+for i in range(0, 5):
+    for j in range(0, 5):
+        # print(i + " " + j)  can't do this in python coz of unsupported type 'int' and 'str'
+        print(str(i)+" "+str(j))
+
+# string methods
+course = "Python For Beginners"
+print(course.upper())
+print(course.find("For"))
+print(course.replace('r', '%'))
+print('python' in course)
+print(10+3)
+print(10/3)     # gives float val
+print(10//3)    # gives integer val
+print(10*3)
+print(4**3)     # power
