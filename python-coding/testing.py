6687973e79a9e5cd3205228985f7a3d51d50f830
Rohuu <rohuu99022@gmail.com>
2023-07-27 23:55:04 +0530

    Python learning: DAY 1

--- python-coding/testing.py
+++ python-coding/testing.py
@@ -21,4 +21,54 @@ print(10+3)
 print(10/3)     # gives float val
 print(10//3)    # gives integer val
 print(10*3)
-print(4**3)     # power
+print(4**3)     # power of
+
+temp = 44
+if (temp == 43):        # or we can write  if(temp is not 43) or if temp is not 43:
+    print("It's 43")
+elif temp > 50:
+    print("Very harsh weather")
+else:
+    print("I've learnt conditionals")
+
+
+# Exercise
+
+weight = int(input("Enter your weight: "))
+unit = input("Is it in Kg or Lbs, press K for kg or L for lbs: ")
+
+if (unit.upper() == 'K'):
+    weight_in_lbs = weight / 0.45
+    print("Your weight in lbs is: " + str(weight_in_lbs))
+else:
+    weight_in_kg = weight * 0.45
+    print("Your weight in kg is: " + str(weight_in_kg))
+
+# multipling number with string
+i = 1
+while i <= 10:
+    # we can multiply any string with number, It will repeat that string
+    print(i*'*')
+    i = i+1
+
+# list
+names = ["Rohit", "Sachin", "Kamal", "Sagar", "Pankaj"]
+print(names)
+print(names[0])
+print(names[1])
+print(names[-1])  # means last index
+print(names[-2])  # means second last index
+names[0] = "Changed_Name"
+print(names)
+
+# prints all elements between [0,3) including first element & excluding second element
+print(names[0:1])
+
+# methods in a list
+nums = [1, 2, 3, 4, 5]
+print(nums)
+nums.append(77)
+nums.append(100)
+print(nums)
+nums.remove(77)     # removes particular item from list
+print(nums)
