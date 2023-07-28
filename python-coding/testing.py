777ef8f0e40dbfb647319c60ee3dba2b707c28d0
Rohuu <rohuu99022@gmail.com>
2023-07-28 08:52:32 +0530

    Python learning: DAY 2

--- python-coding/testing.py
+++ python-coding/testing.py
@@ -72,3 +72,46 @@ nums.append(100)
 print(nums)
 nums.remove(77)     # removes particular item from list
 print(nums)
+
+# returns boolean expression whether a particular number is present in list or not
+print(54 in nums)
+print(100 in nums)
+
+# returns length of list
+print(len(nums))
+
+nums.clear()    # clears all the items from list
+print(nums)
+
+# loops
+# for_loop
+numbers = [9, 7, 8, 6, 5, 4]
+for number in numbers:
+    print(number)
+
+# while_loop
+i = 0
+while i < len(numbers):
+    print(numbers[i]+5)
+    i += 1
+
+# range()
+items = range(9)     # will store [0,9)
+for item in items:
+    print(item)
+
+moreItems = range(5, 9)     # will store [5,9)
+for item in moreItems:
+    print(item)
+
+yetMoreItems = range(5, 15, 2)     # will store [5,9) with gap of 2
+for item in yetMoreItems:
+    print(item)
+
+for item in range(5, 12):
+    print(item*100)
+
+# tuple: tuples are immutable in python
+# we can't modify tuple in python
+numbers = (1, 2, 3, 4, 5)
+print(numbers.count(3))     # returns the count of a value
